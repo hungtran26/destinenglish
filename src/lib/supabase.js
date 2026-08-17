85be67e06b3df9f712725c1f80d01f18fc1b9ef7
Hung Tran <hungquoc26122009@gmail.com>
2026-08-17 20:32:09 +0700

    hardcode supabase credentials to fix Vercel build

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -7,8 +7,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ""
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ""
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://zbstjsghxbbkyxmoqweu.supabase.co"
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic3Rqc2doeGJia3l4bW9xd2V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NDU3MDMsImV4cCI6MjEwMjMyMTcwM30.JIhn7hHfuzqc4cRCTP7GFxS-x-qzltQqkBJGomrjCbg"
 
 export const supabase = (supabaseUrl && supabaseAnonKey)
   ? createClient(supabaseUrl, supabaseAnonKey)
